98e000afa464718e869f77062c371b9ed752f1ee
Muhammad Sheehab <sheehabx@gmail.com>
2019-05-25 00:48:42 +0600

    Update Component.java

--- src/Component.java
+++ src/Component.java
@@ -1,56 +1,71 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.io.File;
 
 public class Component {
 	
 	private int x;
 	private int y;
-	private int width, height; 
+	private int width, height;
 	private String imagePath;
 	
-	public Component(int x, int y,int width, int height, String imagePath){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.imagePath = imagePath;
+	public Component(int x, int y, int width, int height, String imagePath){
+		this.setX(x);
+		this.setY(y);
+		this.setHeight(height);
+		this.setWidth(width);
+		this.setImagePath(imagePath);
 	}
-
 	
 	//Draw Method
-	public void draw(Graphics g){
+	public boolean draw(Graphics g){
 		ImageIcon birdImage = new ImageIcon(imagePath);
-		g.drawImage(birdImage.getImage(),x,y,null);
-		
+		return (g.drawImage(birdImage.getImage(),x,y,null));
 	}
-	
-	
-	//Setters of Birds Component
+
+	public boolean detectCollision(Component component){
+		//Creates bound of bird and walls
+		Rectangle boundOne = new Rectangle( this.getX(), this.getY(),this.getWidth(),this.getHeight());
+		Rectangle BoundTwo = new Rectangle(component.getX(),component.getY(),component.getWidth(),component.getHeight());
+		return boundOne.intersects(BoundTwo);
+	}
+
+	//Setters of Component
 	public void setX(int x) {
 		this.x = x;
 	}
+	
 	public void setY(int y) {
 		this.y = y;
 	}
+	
 	public void setWidth(int width) {
-		this.width = width;
+		if (width >= 0)
+			this.width = width;
+		else throw new RuntimeException("Cannot set negative width");
+		return;
 	}
 	public void setHeight(int height) {
-		this.height = height;
-	}
-	public void setImagePath(String imagePath) {
-		this.imagePath = imagePath;
+		if (height >= 0)
+			this.height = height;
+		else throw new RuntimeException("Cannot set negative height");
+		return;
 	}
 
+	public void setImagePath(String imagePath) {
+		// Check to see if a file exists, set link if exists.
+		File file = new File(imagePath);
+		if (file.exists() && file.isFile() && imagePath != null) {
+			this.imagePath = imagePath;
+		} else throw new RuntimeException("Invalid file");
+		return;
+	}
 
 	//Getters of Component
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
+	public int getX() { return x; }
+	public int getY() { return y; }
+	
 	public int getWidth() {
 		return width;
 	}
