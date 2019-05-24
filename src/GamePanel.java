744d24c6d20a21bd5425f7758c6d33fa27094fe3
Muhammad Sheehab <sheehabx@gmail.com>
2019-05-25 00:49:36 +0600

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,31 +1,39 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GamePanel  extends JPanel implements KeyListener {
 
 	private static final long serialVersionUID = 1L;
-	
-	public int WIDTH = 706, HEIGHT = 490;
-	private double score = 0; 
+
+	/**
+	 * Game sounds initialization, fixes after testing the GameSound Class
+	 *
+	 */
+	protected GameSound dieSound = new GameSound("sounds//die.wav");
+	protected GameSound pointSound = new GameSound("sounds//point.wav");
+	protected GameSound flapSound = new GameSound("sounds//flap.wav");
+
+	public final int WIDTH = 700, HEIGHT = 490;
+	protected double points = 0;
 	
 	//background
-	private Component background1 = new Component(0, 0, 750, 490, "images//background.png");
-	private Component background2 = new Component(700, 0, 750, 490, "images//background.png");
+	protected Component background1 = new Component(0, 0, 700, 490, "images//background.png");
+	protected Component background2 = new Component(700, 0, 700, 490, "images//background.png");
+
 	//Bird
-	private Component bird = new Component((WIDTH/ 2 - 150), (HEIGHT /2) - 40, 50 , 36, "images//bird.png");
+	protected Component bird = new Component((WIDTH/ 2 - 150), (HEIGHT /2) - 40, 25 , 25, "images//bird.png");
+
 	//Wall array
-	private Component[] walls = new Component[8];
+	protected Component[] walls = new Component[8];
+
 	//Ground
-	private Component ground1 = new Component(0, 445, 750, 490, "images//ground.png");
-	private Component ground2 = new Component(700, 445, 750, 490, "images//ground.png");
-	//Wall speed
-	private int speed = 2;
+	protected Component ground1 = new Component(0, 445, 710, 490, "images//ground.png");
+	protected Component ground2 = new Component(705, 445, 710, 490, "images//ground.png");
+
 	//Game Controller
-	private boolean started = false, gameOver = false; 
-	
+	protected boolean started = false, gameOver = false;
 	
 	//GamePanel Constructor
 	public GamePanel(){
@@ -34,70 +42,48 @@ public class GamePanel  extends JPanel implements KeyListener {
 	}
 	
 	//Create the wall and
-	public void Start(){
+	public void initializeGameComponents(){
 		createWalls();
 	}
-	
-	
+
 	//Gives bird Some Gravity
-	public void Gravity(){	
-		
-		bird.setY(bird.getY() + 3);						//Starts falling by One Pixel
-		
-		try{ Thread.sleep(3); } catch (Exception e) {}
-		
-		// checks if it touches the ground or Sky
-		if(bird.getY() > 450 || bird.getY() <= 0){ 
-			// Plays hit Sound and if bird touches anything then makes gameOver true.
-			if(!gameOver) GameSound.hitSound();   
-			gameOver = true;
-			bird.setX(-500);
-		}
-		
-		super.repaint();
+	public void gravityPull(){
+		//Starts falling by One Pixel
+		bird.setY(bird.getY() + 2);
 	}
 	
-	
 	//Helps wall to shift from left to right 
-	public void scrollWalls(){ 
-		
-		for(int i = 0; i < walls.length; i+=2){
-			if(walls[i].getX() > - 150){
-				walls[i].setX(walls[i].getX() - speed);
-				walls[i+1].setX(walls[i+1].getX() - speed);
-			}
-			else{
+	public void scrollWalls(){
+		for(int i = 0; i < walls.length; i+=2) {
+			if (walls[i].getX() > -150) {
+				walls[i].setX(walls[i].getX() - 2);
+				walls[i + 1].setX(walls[i + 1].getX() - 2);
+			} else {
 				walls[i].setX(700);
-				walls[i+1].setX(700);
-					
+				walls[i + 1].setX(700);
 			}
 		}
-		
-		
-		try{ Thread.sleep(5); }  catch(Exception e) {}
-		
-		super.repaint();
 	}
 	
-	
-	
 	//Helps wall to shift from left to right 
-	public void scrollBG(){ 
-			
+	public void scrollBG(){
+
 		//Moves First BG Image
 		if(background1.getX() > -700){
-			background1.setX(background1.getX() - speed/2);
+			background1.setX(background1.getX() - 1);
 		} else { background1.setX(700);	}
 		
 		//Moves Second BG Image
 		if(background2.getX() > -700){
-			background2.setX(background2.getX() -speed/2);
+			background2.setX(background2.getX() -1);
 		} else { background2.setX(700);	}
-		
-		try{ Thread.sleep(5); }  catch(Exception e) {}
-		super.repaint();
+
 	}
-	
+
+    //Helps wall to shift from left to right
+    public void flap(){
+        bird.setY(bird.getY() - 6);
+    }
 	
 	
 	//Helps Ground to shift from left to right 
@@ -105,20 +91,16 @@ public class GamePanel  extends JPanel implements KeyListener {
 		
 		//Moves First ground Image
 		if(ground1.getX() > -700){
-			ground1.setX(ground1.getX() - speed);
+			ground1.setX(ground1.getX() - 2);
 		} else {  ground1.setX(700); }
 		
-		//Moves First ground Image
+		//Moves Second ground Image
 		if(ground2.getX() > -700){	
-			ground2.setX(ground2.getX() -speed);
+			ground2.setX(ground2.getX() -2);
 		} else {  ground2.setX(700); }
-		
-		//interval
-		try{ Thread.sleep(5); }  catch(Exception e) {}
-		super.repaint();
 	}
-	
-	
+
+
 	//Draws All Components on panel
 	public void paint(Graphics g){
 		
@@ -128,12 +110,11 @@ public class GamePanel  extends JPanel implements KeyListener {
 		
 		//Draws Bird
 		bird.draw(g);
-		
+
 		//Draws Walls
 		for(int i = 0; i < walls.length; i++){
 			walls[i].draw(g);
 		}
-	
 		////Draws Texts
 		if (gameOver) {	
 			g.setFont(new Font("Arial", 1, 40));
@@ -150,36 +131,29 @@ public class GamePanel  extends JPanel implements KeyListener {
 		else if (!gameOver && started){	
 			g.setColor(Color.green);
 			g.setFont(new Font("Arial", 1, 20));
-			g.drawString(String.valueOf("Score:"+score), 10 , 25);
+			g.drawString(String.valueOf("Score:"+points), 10 , 25);
 		}
-		
+
 		//Draws Grounds
 		ground1.draw(g);
 		ground2.draw(g);
-		
 	}
-	
-	
+
 	//Checks Collisions between wall and bird
-	public void checkCollision(){
-		
-		//Creates bound of bird
-		Rectangle birdBound = new Rectangle( bird.getX(), bird.getY(),25,25);
-		
+	public void collisionOrScore(){
 		//Creates bound of all walls
-		for(int i = 0; i < walls.length; i++){
-			Rectangle WallRect=new Rectangle(walls[i].getX(),walls[i].getY(),walls[i].getWidth(),walls[i].getHeight());
-			if(WallRect.intersects(birdBound)==true){
+		for(int i = 0; i < walls.length; i++) {
+			if (bird.detectCollision(walls[i]) || bird.getY() > 450 || bird.getY() <= 0) {
 				bird.setX(-500);
-				if(!gameOver) GameSound.hitSound();
+				if (!gameOver) dieSound.playAudioFeedback();
 				gameOver = true;
 				break;
-			} else if(bird.getX() == walls[i].getX()+1){
+			} else if (bird.getX() == walls[i].getX()) {
 				//if bird passes one wall then counts a point
-				score();
-			}	
+				this.points = incrementScore(this.points, 0.5);
+				pointSound.playAudioFeedback();
+			}
 		}
-		
 	}
 	
 	
@@ -188,73 +162,49 @@ public class GamePanel  extends JPanel implements KeyListener {
 		
 		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_SPACE){
 			//Starts the game
+
 			if(!started) {
 				started = true;
 			}
-			
+
 			//Restarts The Game if its Over
 			if(gameOver){
 				bird.setY((HEIGHT /2));
 				bird.setX(WIDTH/2 - 150);
-				score = 0;
+				points = 0;
 				gameOver = false;
 			}
-				
+
 			//Flaps if key pressed
-			BirdFlap flap = new BirdFlap(this,bird);
+			BirdFlap flap = new BirdFlap(this);
 			flap.start();
-			GameSound.flapSound();
+			flapSound.playAudioFeedback();
 		}
 	
 	}
 	
-	
 	//Increments the score
-	public void score(){
-		score+= 0.5;
-		GameSound.pointSound();
+	public double incrementScore(double points, double amount){
+		if(points >= 0)
+			points += amount;
+		return points;
 	}
 	
 	public void createWalls(){
-		int i = 0;
-		int x = 600; // First walls distance from very right side
-		
+
+		int initialPipeDistance = 600;
 		//walls width and height, width is static and height changes constantly
-		int width = 60, height = 280; 
-		
-		
-		//1st & 2nd wall
-		walls[i] = new Component(x, 280, width, height, "images//wallUp.png");
-		i++;
-		walls[i] = new Component(x, -90, width, height, "images//wallDown.png");
-		x = walls[i].getX()+220; // 160 is the difference between wall
-		i++;
-		
-		//3rd & 4th wall
-		walls[i] = new Component(x, 360, width, height, "images//wallUp.png");
-		i++;
-		walls[i] = new Component(x, -10, width, height, "images//wallDown.png");
-		x = walls[i].getX()+220;
-		i++;
-		
-		
-		//5rd & 6th wall
-		walls[i] = new Component(x, 280, width, height, "images//wallUp.png");
-		i++;
-		walls[i] = new Component(x, -90, width, height, "images//wallDown.png");
-		x = walls[i].getX()+220;
-		i++;
-		
-		
-		//7rd & 8th wall
-		walls[i] = new Component(x, 220, width, height, "images//wallUp.png");
-		i++;
-		walls[i] = new Component(x, -170, width, height, "images//wallDown.png");
-		x = walls[i].getX()+220;
-		
+		int width = 60, height = 280;
+		for(int i = 0; i < walls.length; i++){
+			int y = ThreadLocalRandom.current().nextInt(180, 360);
+			walls[i] = new Component(initialPipeDistance, y, width, height, "images//wallUp.png");
+			i++;
+			walls[i] = new Component(initialPipeDistance, y-370, width, height, "images//wallDown.png");
+			initialPipeDistance = initialPipeDistance + 220;
+		}
 	}
 	
-	//Setters anmd Getters
+	//Setters and Getters
 	public boolean isStarted() {
 		return started;
 	}
@@ -270,7 +220,6 @@ public class GamePanel  extends JPanel implements KeyListener {
 	public void setGameOver(boolean gameOver) {
 		this.gameOver = gameOver;
 	}
-	
 	
 	//Key Events
 	@Override
